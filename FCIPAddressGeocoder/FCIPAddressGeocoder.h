38be38002b3a27322850a7e0b4536c9df404307a
Fabio Caccamo <fabio.caccamo@gmail.com>
2014-07-08 13:05:51 +0200

    added source code

---
+++ FCIPAddressGeocoder/FCIPAddressGeocoder.h
@@ -0,0 +1,34 @@
+//
+//  FCIPAddressGeocoder.h
+//
+//  Created by Fabio Caccamo on 07/07/14.
+//  Copyright (c) 2014 Fabio Caccamo - http://www.fabiocaccamo.com/ - All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface FCIPAddressGeocoder : NSObject
+{
+    NSURL *_url;
+    NSURLRequest *_request;
+    NSOperationQueue *_operationQueue;
+    void (^_completionHandler)(BOOL success);
+}
+
+@property (nonatomic, readonly, getter = isGeocoding) BOOL geocoding;
+
+@property (nonatomic, readonly, strong) NSError *error;
+
+@property (nonatomic, readonly, copy) NSDictionary *locationInfo;
+@property (nonatomic, readonly, copy) CLLocation *location;
+@property (nonatomic, readonly, copy) NSString *locationCity;
+@property (nonatomic, readonly, copy) NSString *locationCountry;
+@property (nonatomic, readonly, copy) NSString *locationCountryCode;
+
+-(void)cancelGeocode;
+-(void)geocode:(void(^)(BOOL success))completionHandler;
+
++(FCIPGeocoder *)sharedGeocoder;
+
+@end
---
+++ FCIPAddressGeocoder/FCIPAddressGeocoder.m
@@ -0,0 +1,122 @@
+//
+//  FCIPAddressGeocoder.m
+//
+//  Created by Fabio Caccamo on 07/07/14.
+//  Copyright (c) 2014 Fabio Caccamo - http://www.fabiocaccamo.com/ - All rights reserved.
+//
+
+#import "FCIPAddressGeocoder.h"
+
+@implementation FCIPAddressGeocoder : NSObject
+
+
+-(id)init
+{
+    self = [super init];
+    
+    if( self )
+    {
+        _url = [NSURL URLWithString:@"http://freegeoip.net/json/"];
+        _request = [NSURLRequest requestWithURL:_url];
+        _operationQueue = [NSOperationQueue new];
+    }
+    
+    return self;
+}
+
+
+-(void)cancelGeocode
+{
+    [_operationQueue cancelAllOperations];
+    
+    _completionHandler = nil;
+    
+    _geocoding = NO;
+    
+    _error = nil;
+    
+    _locationInfo = nil;
+    _location = nil;
+    _locationCity = nil;
+    _locationCountry = nil;
+    _locationCountryCode = nil;
+}
+
+
+-(void)geocode:(void (^)(BOOL))completionHandler
+{
+    if( _location != nil && [[_location.timestamp dateByAddingTimeInterval:60] timeIntervalSinceNow] > 0 )
+    {
+        if( completionHandler ){
+            completionHandler( YES );
+        }
+    }
+    
+    [self cancelGeocode];
+    
+    _completionHandler = completionHandler;
+    
+    _geocoding = YES;
+    
+    [NSURLConnection sendAsynchronousRequest:_request queue:_operationQueue completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        
+        if( connectionError == nil )
+        {
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+            
+            if( httpResponse.statusCode == 200 )
+            {
+                NSError *JSONError = nil;
+                NSDictionary *JSONData = (NSDictionary *)[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&JSONError];
+                
+                if( JSONError == nil )
+                {
+                    _locationInfo = JSONData;
+                    
+                    CLLocationDegrees latitude = [[_locationInfo objectForKey:@"latitude"] doubleValue];
+                    CLLocationDegrees longitude = [[_locationInfo objectForKey:@"longitude"] doubleValue];
+                    
+                    _location = [[CLLocation alloc] initWithLatitude:latitude longitude:longitude];
+                    _locationCity = [_locationInfo objectForKey:@"city"];
+                    _locationCountry = [_locationInfo objectForKey:@"country_name"];
+                    _locationCountryCode = [_locationInfo objectForKey:@"country_code"];
+                }
+                else {
+                    _error = JSONError;
+                    //NSLog(@"JSON error");
+                }
+            }
+            else {
+                _error = [NSError errorWithDomain:@"" code:kCLErrorNetwork userInfo:nil];
+                //NSLog(@"httpResponse.statusCode error (%i)\nMaybe the service is unavailable due to maintenance or high load. Check it: http://freegeoip.net/", httpResponse.statusCode);
+            }
+        }
+        else {
+            _error = connectionError;
+            //NSLog(@"connection error");
+        }
+        
+        _geocoding = NO;
+        
+        if( _completionHandler )
+        {
+            _completionHandler( _error == nil );
+        }
+    }];
+}
+
+
++(FCIPAddressGeocoder *)sharedGeocoder
+{
+    static FCIPAddressGeocoder *instance = nil;
+    static dispatch_once_t token;
+    
+    dispatch_once(&token, ^{
+        instance = [[self alloc] init];
+    });
+    
+    return instance;
+}
+
+
+@end
